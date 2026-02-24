9beecc1cccf07a1879a3dc1a85b039ad4cf9f9b2
KingRuby14 <selvapandi322@gmail.com>
2026-02-24 08:28:04 +0530

    Google Login

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,100 @@
 import Colors from "@/services/Colors";
-import { StyleSheet, Text, View, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
 import { useEffect } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
 
 export default function Index() {
   const navigation = useNavigation();
+
   useEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+
+  WebBrowser.maybeCompleteAuthSession();
+
+  const [request, response, promptAsync] =
+    Google.useAuthRequest({
+      webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+      androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
+    });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      console.log("LOGIN SUCCESS");
+      console.log(response.authentication);
+    }
+  }, [response]);
+
   return (
-    <View
-      style={styles.container}
-    >
-      <Image style={styles.imagestyle} source={require('../assets/images/logo.png')} />
-      <Text style={styles.headeing}>Welcome to </Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.imagestyle}
+        source={require("../assets/images/logo.png")}
+      />
+
+      <Text style={styles.headeing}>Welcome to</Text>
       <Text style={styles.headeing}>Expenses Tracker</Text>
+
       <View style={styles.CONTAINER2}>
-        <Text style={{ fontSize: 18, color: Colors.black, textAlign: "center" }}>
+        <Text
+          style={{
+            fontSize: 18,
+            color: Colors.black,
+            textAlign: "center",
+          }}
+        >
           Track your expenses and manage your budget with ease.
         </Text>
-        <View style={[styles.button, { display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: 3 }]}>
-          <Image source={require('../assets/images/google.png')} style={{ width: 20, height: 20, marginRight: 10 }} />
-          <Text style={styles.google}>Sign In With Google</Text>
-        </View>
-        <View style={[styles.button, { backgroundColor: Colors.logo, borderColor: Colors.logo }]}>
-          <Text style={[styles.google, { color: Colors.white }]}>Skip</Text>
-        </View>
+
+        {/* GOOGLE LOGIN BUTTON */}
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            },
+          ]}
+          onPress={() => promptAsync()}
+          disabled={!request}
+        >
+          <Image
+            source={require("../assets/images/google.png")}
+            style={{ width: 20, height: 20, marginRight: 10 }}
+          />
+          <Text style={styles.google}>
+            Sign In With Google
+          </Text>
+        </TouchableOpacity>
+
+        {/* SKIP BUTTON */}
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              backgroundColor: Colors.logo,
+              borderColor: Colors.logo,
+            },
+          ]}
+        >
+          <Text
+            style={[styles.google, { color: Colors.white }]}
+          >
+            Skip
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -46,7 +113,7 @@ const styles = StyleSheet.create({
   },
   headeing: {
     fontSize: 30,
-    fontFamily: 'monstreater-bold',
+    fontFamily: "monstreater-bold",
     fontWeight: "bold",
     color: Colors.logo,
     textAlign: "center",
@@ -73,5 +140,5 @@ const styles = StyleSheet.create({
   google: {
     fontSize: 15,
     textAlign: "center",
-  }
-})
+  },
+});
