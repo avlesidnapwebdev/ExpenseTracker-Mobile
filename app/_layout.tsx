d4074ce6ebd9bd87fb0b9e2705575fad786fed2a
KingRuby14 <selvapandi322@gmail.com>
2026-02-27 22:05:48 +0530

    undo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,6 @@ import { AuthProvider } from "../context/AuthContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-/* 🔥 PRODUCTION MODE */
-if (!__DEV__) {
-  console.log = () => { };
-  console.warn = () => { };
-}
-
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
